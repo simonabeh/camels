6d4b4264a2b1dfb01da5993d747f6db22bad136f
Geraud Lemerrer <geraud.lemerrer@gmail.com>
2019-05-24 12:47:14 +0200

    creation of new and form

--- app/models/camel.rb
+++ app/models/camel.rb
@@ -1,3 +1,11 @@
 class Camel < ApplicationRecord
   belongs_to :user
+  CATEGORY = ['Apartment', 'City', 'Desert', 'Mountain', 'Speed Race',
+  'Water', 'Friendly Camel', 'Horny Camel', 'Fancy Camel', 'Swaggy Camel']
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  validates :location, presence: true
+  # validates :price, presence: true
 end
