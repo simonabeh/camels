ab9a8afc555619ea72717c30edadb26b6c6dfd33
alexandre auriol <auriol.alexandre@gmail.com>
2019-05-24 17:17:44 +0200

    create bookings/modification datetime >> date

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_121645) do
+ActiveRecord::Schema.define(version: 2019_05_24_144528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "date"
+    t.date "date"
     t.bigint "user_id"
     t.bigint "camel_id"
     t.datetime "created_at", null: false
